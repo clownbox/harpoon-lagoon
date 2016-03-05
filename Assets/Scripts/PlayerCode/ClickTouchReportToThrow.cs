5fa284239c4a36c8233edb5573ab046ad7fb3e09
Chris DeLeon <gamkedo@gmail.com>
2016-03-04 16:23:17 -0800

    Boat tilt from weather now affects throw accuracy, causes aim line to waver

--- Assets/Scripts/PlayerCode/ClickTouchReportToThrow.cs
+++ Assets/Scripts/PlayerCode/ClickTouchReportToThrow.cs
@@ -14,6 +14,7 @@ public class ClickTouchReportToThrow : MonoBehaviour {
 		}
 
 		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
 		RaycastHit rhInfo;
 		if(Physics.Raycast(ray, out rhInfo)) {
 			if(rhInfo.collider.gameObject != gameObject) {
@@ -24,15 +25,6 @@ public class ClickTouchReportToThrow : MonoBehaviour {
 				return;
 			}
 
-			if( (throwUponReleaseNotPress == true && Input.GetButtonUp("Fire1")) ||
-				(throwUponReleaseNotPress == false && Input.GetButtonDown("Fire1"))) {
-
-				if( (rhInfo.collider.gameObject.layer == LayerMask.NameToLayer("WaterTouch") && pullbackNotAimDown==false) ||
-					(rhInfo.collider.gameObject.layer == LayerMask.NameToLayer("SkyTouch") && pullbackNotAimDown==true)) {
-
-					htScript.ThrowAt(rhInfo.point);
-				}
-			}
 			DashedLine.startVertex = Camera.main.WorldToScreenPoint(htScript.transform.position);
 			DashedLine.startVertex.x /= Screen.width;
 			DashedLine.startVertex.y /= Screen.height;
@@ -40,12 +32,49 @@ public class ClickTouchReportToThrow : MonoBehaviour {
 			Vector3 mousePos = Input.mousePosition;
 			Vector3 atPt = new Vector3(mousePos.x / Screen.width, mousePos.y / Screen.height, 0);
 
+			float boatTilt = htScript.transform.rotation.eulerAngles.z;
+			if(boatTilt > 180.0f) {
+				boatTilt = 360.0f + boatTilt; // turn high positive into negative, so that...
+			}
+			boatTilt *= 2.5f * Mathf.Deg2Rad; // linear multiplier will scale it correctly
+
+			// translate point back to origin:
+			atPt -= DashedLine.startVertex;
+
+			// rotate point
+			float s = Mathf.Sin(boatTilt);
+			float c = Mathf.Cos(boatTilt);
+			float xnew = atPt.x * c - atPt.y * s;
+			float ynew = atPt.x * s + atPt.y * c;
+			atPt.x = xnew;
+			atPt.y = ynew;
+
+			atPt += DashedLine.startVertex;
+
 			if(fixedLength) {
 				Vector3 ptDelta = atPt - DashedLine.startVertex;
 				DashedLine.endVertex = DashedLine.startVertex + ptDelta.normalized * 1.0f;
 			} else {
 				DashedLine.endVertex = atPt;
 			}
+
+			if( (throwUponReleaseNotPress == true && Input.GetButtonUp("Fire1")) ||
+				(throwUponReleaseNotPress == false && Input.GetButtonDown("Fire1"))) {
+
+				if( (rhInfo.collider.gameObject.layer == LayerMask.NameToLayer("WaterTouch") && pullbackNotAimDown==false) ||
+					(rhInfo.collider.gameObject.layer == LayerMask.NameToLayer("SkyTouch") && pullbackNotAimDown==true)) {
+
+					Vector3 rotatedTarget = rhInfo.point;
+					rotatedTarget -= htScript.transform.position;
+					xnew = rotatedTarget.x * c - rotatedTarget.y * s;
+					ynew = rotatedTarget.x * s + rotatedTarget.y * c;
+					rotatedTarget.x = xnew;
+					rotatedTarget.y = ynew;
+					rotatedTarget += htScript.transform.position;
+					htScript.ThrowAt(rotatedTarget);
+				}
+			}
+
 		} else {
 			DashedLine.startVertex = DashedLine.endVertex = -1000.0f * Vector3.one;
 		}
