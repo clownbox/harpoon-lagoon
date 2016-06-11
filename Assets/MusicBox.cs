888a95fbbae78b7e4d831c693c3a95f0615c2e12
Chris DeLeon <gamkedo@gmail.com>
2016-06-10 19:55:46 -0700

    Thomas music dropped in (realized after some fiddling I needed to re-add it to a bank due to merge with newer Nate fspro project file, hoping I didn't smash over any custom music properties in the process)

---
+++ Assets/MusicBox.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicBox : MonoBehaviour {
+	private FMOD.Studio.EventInstance songEvt;
+
+	// Use this for initialization
+	void Start () {
+		songEvt = FMODUnity.RuntimeManager.CreateInstance("event:/music");
+		songEvt.start();
+	}
+}
